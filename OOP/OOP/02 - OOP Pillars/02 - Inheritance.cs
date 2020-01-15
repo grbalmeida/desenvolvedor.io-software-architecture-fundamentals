23782fc1f990122e176a6cc01773fef628391bd0
gilvan <g.r.almeida@live.com>
2020-01-14 21:36:31 -0300

    Inheritance

---
+++ OOP/OOP/02 - OOP Pillars/02 - Inheritance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Employee : Person
+    {
+        public DateTime AdmissionDate { get; set; }
+        public string Record { get; set; }
+    }
+
+    public class Process
+    {
+        public void Execution()
+        {
+            var employee = new Employee
+            {
+                Name = "João da Silva",
+                DateOfBirth = Convert.ToDateTime("1990/01/01"),
+                AdmissionDate = DateTime.Now,
+                Record = "0123456"
+            };
+
+            var employeeAge = employee.CalculateAge();
+        }
+    }
+}
