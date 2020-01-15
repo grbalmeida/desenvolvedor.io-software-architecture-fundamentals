b079f4076eecd0f59a189c3892f1e735d2dee59c
gilvan <g.r.almeida@live.com>
2020-01-14 21:23:44 -0300

    State and behavior

---
+++ OOP/OOP/01 - Class x Object/Home.cs
@@ -0,0 +1,26 @@
+﻿namespace OOP
+{
+    // Class definition
+    public class Home
+    {
+        public int SizeSquareMeters { get; set; }
+        public int Floors { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfParkingSpaces { get; set; }
+    }
+
+    public class Object
+    {
+        public Object()
+        {
+            // Object definition
+            var home = new Home
+            {
+                SizeSquareMeters = 100,
+                Floors = 2,
+                Price = 100000,
+                NumberOfParkingSpaces = 3
+            };
+        }
+    }
+}
---
+++ OOP/OOP/02 - OOP Pillars/StateBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int CalculateAge()
+        {
+            var currentDate = DateTime.Now;
+            var age = currentDate.Year - DateOfBirth.Year;
+
+            if (currentDate < DateOfBirth.AddYears(age)) age--;
+
+            return age;
+        }
+    }
+}
