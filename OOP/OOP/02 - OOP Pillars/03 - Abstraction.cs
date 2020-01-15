843f3d23012a5dab4689a4e0a26d47e32d1beaae
gilvan <g.r.almeida@live.com>
2020-01-14 22:11:15 -0300

    Abstraction

---
+++ OOP/OOP/02 - OOP Pillars/03 - Abstraction.cs
@@ -0,0 +1,17 @@
+﻿namespace OOP
+{
+    public abstract class Appliance
+    {
+        private readonly string _name;
+        private readonly int _voltage;
+
+        protected Appliance(string name, int voltage)
+        {
+            _name = name;
+            _voltage = voltage;
+        }
+
+        public abstract void TurnOn();
+        public abstract void TurnOff();
+    }
+}
