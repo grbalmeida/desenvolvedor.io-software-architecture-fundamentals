86540103b04409f10751ac4d101e24ca6469c21f
gilvan <g.r.almeida@live.com>
2020-01-15 21:00:04 -0300

    Interface vs Implementation

---
+++ OOP/OOP/03 - Interface vs Implementation/Cases.cs
@@ -0,0 +1,67 @@
+﻿namespace OOP
+{
+    public interface IRepository
+    {
+        void Add();
+    }
+
+    public class Repository : IRepository
+    {
+        //public Repository(int a)
+        //{
+
+        //}
+
+        public void Add()
+        {
+            // Add item
+        }
+    }
+
+    public class FakeRepository : IRepository
+    {
+        public void Add()
+        {
+            // Add Item
+        }
+    }
+
+    public class ImplementationUse
+    {
+        public void Process()
+        {
+            var repository = new Repository(); // high coupling
+            repository.Add();
+        }
+    }
+
+    public class AbstractionUse
+    {
+        private readonly IRepository _repository;
+
+        public AbstractionUse(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Process()
+        {
+            _repository.Add();
+        }
+    }
+
+    public class InterfaceImplementationTest
+    {
+        public InterfaceImplementationTest()
+        {
+            var implementationRepository = new ImplementationUse();
+            implementationRepository.Process();
+
+            var abstractionRepository = new AbstractionUse(new Repository());
+            abstractionRepository.Process();
+
+            var abstractionFakeRepository = new AbstractionUse(new FakeRepository());
+            abstractionFakeRepository.Process();
+        }
+    }
+}
