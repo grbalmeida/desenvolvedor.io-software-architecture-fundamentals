4f0dfb06fc0f0e1d14fc3e69332b56bc16c10a1e
gilvan <g.r.almeida@live.com>
2020-01-18 22:12:33 -0300

    SRP - Single Responsability Principle

---
+++ SOLID/SOLID/1 - SRP/SRP.Solution/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿namespace SOLID.SRP.Solution
+{
+    public class CustomerService
+    {
+        public string AddCustomer(Customer customer)
+        {
+            if (!customer.Validate())
+            {
+                return "Invalid data";
+            }
+
+            var customerRepository = new CustomerRepository();
+            customerRepository.AddCustomer(customer);
+
+            EmailServices.Send(
+                "company@company.com",
+                customer.Email.Address,
+                "Welcome", "Congratulations are registered"
+            );
+
+            return "Customer successfully registered";
+        }
+    }
+}
---
+++ SOLID/SOLID/1 - SRP/SRP.Violation/Customer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Mail;
+
+namespace SOLID.SRP.Violation
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public string AddCustomer()
+        {
+            if (!Email.Contains("@"))
+            {
+                return "Customer with invalid email";
+            }
+
+            if (CPF.Length != 11)
+            {
+                return "Customer with invalid CPF";
+            }
+
+            using (var sqlConnection = new SqlConnection())
+            {
+                var sqlCommand = new SqlCommand();
+
+                sqlConnection.ConnectionString = "MyConnectionString";
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = "INSERT INTO CUSTOMER (NAME, EMAIL, CPF, CREATIONDATE) VALUES (@name, @email, @cpf, @creationDate)";
+
+                sqlCommand.Parameters.AddWithValue("name", Name);
+                sqlCommand.Parameters.AddWithValue("email", Email);
+                sqlCommand.Parameters.AddWithValue("cpf", CPF);
+                sqlCommand.Parameters.AddWithValue("creationDate", CreationDate);
+
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+
+            var mail = new MailMessage("company@company.com", Email);
+            var client = new SmtpClient
+            {
+                Port = 25,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Host = "smtp.google.com"
+            };
+
+            mail.Subject = "Welcome.";
+            mail.Body = "Congratulations! You are registered.";
+            client.Send(mail);
+
+            return "Customer successfully registered!";
+        }
+    }
+}
