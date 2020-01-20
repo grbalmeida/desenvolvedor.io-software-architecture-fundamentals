fcb6129595d6bad9355440e76b361dea8b53592d
gilvan <g.r.almeida@live.com>
2020-01-20 19:48:14 -0300

    'Real World' example

---
+++ DI/DemoDI/DemoDI/Cases/RealLife.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DemoDI.Cases
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+
+    public interface ICustomerRepository
+    {
+        void AddCustomer(Customer customer);
+    }
+
+    public class CustomerRepository : ICustomerRepository
+    {
+        public void AddCustomer(Customer customer)
+        {
+            // Do something
+        }
+    }
+
+    public interface ICustomerServices
+    {
+        void AddCustomer(Customer customer);
+    }
+
+    public class CustomerServices : ICustomerServices
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerServices(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            _customerRepository.AddCustomer(customer);   
+        }
+    }
+}
---
+++ DI/DemoDI/DemoDI/Controllers/RealLifeController.cs
@@ -0,0 +1,20 @@
+﻿using DemoDI.Cases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoDI.Controllers
+{
+    public class RealLifeController : Controller
+    {
+        private readonly ICustomerServices _customerServices;
+
+        public RealLifeController(ICustomerServices customerServices)
+        {
+            _customerServices = customerServices;
+        }
+
+        public void Index()
+        {
+            _customerServices.AddCustomer(new Customer());
+        }
+    }
+}
