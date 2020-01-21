a68c64c0e0113f0c28d2a77641a8308afa088588
gilvan <g.r.almeida@live.com>
2020-01-20 21:25:25 -0300

    Property Injection

---
+++ DI/DemoDI/DemoDI/Controllers/FromServicesController.cs
@@ -0,0 +1,13 @@
+﻿using DemoDI.Cases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoDI.Controllers
+{
+    public class FromServicesController : Controller
+    {
+        public void Index([FromServices] ICustomerServices customerServices)
+        {
+            customerServices.AddCustomer(new Customer());
+        }
+    }
+}
