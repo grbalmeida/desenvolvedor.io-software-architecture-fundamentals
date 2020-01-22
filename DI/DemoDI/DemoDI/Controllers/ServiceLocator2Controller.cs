050fd0dc245288cd899e4f9387dc76e4300b91e7
gilvan <g.r.almeida@live.com>
2020-01-21 22:26:03 -0300

    Service Locator 'Pattern'

---
+++ DI/DemoDI/DemoDI/Controllers/ServiceLocator2Controller.cs
@@ -0,0 +1,16 @@
+﻿using DemoDI.Cases;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DemoDI.Controllers
+{
+    public class ServiceLocator2Controller : Controller
+    {
+        public void Index([FromServices] IServiceProvider serviceProvider)
+        {
+            // Returns null if not registered
+            serviceProvider.GetRequiredService<ICustomerServices>().AddCustomer(new Customer());
+        }
+    }
+}
---
+++ DI/DemoDI/DemoDI/Controllers/ServiceLocatorController.cs
@@ -0,0 +1,23 @@
+﻿using DemoDI.Cases;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DemoDI.Controllers
+{
+    public class ServiceLocatorController : Controller
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ServiceLocatorController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Index()
+        {
+            // Returns null if not registered
+            _serviceProvider.GetRequiredService<ICustomerServices>().AddCustomer(new Customer());
+        }
+    }
+}
